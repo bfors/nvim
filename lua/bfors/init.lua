3de33cc2c929f356f745743ec24fdb749808f872
Brian Forsberg <bforsberg15@gmail.com>
2024-02-09 10:34:49 -0600

    Setup harpoon keys and terminal exit shortcut.

--- lua/bfors/init.lua
+++ lua/bfors/init.lua
@@ -40,8 +40,10 @@ vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:lis
 vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
--- vim.keymap.set("n", "<C-a>", function() harpoon:list():select(7) end)
+vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
 --
 -- -- Toggle previous & next buffers stored within Harpoon list
--- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
--- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+vim.keymap.set({ 't' }, '<ESC>', [[<C-\><C-n>]], { desc = '' })
