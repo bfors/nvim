26660b5246903cdba94be7646fa2fbd838250e20
Brian Forsberg <bforsberg15@gmail.com>
2024-02-26 13:34:37 -0600

    Update to the latest kickstart defaults and reapply my customization.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,12 +3,29 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			'nvim-telescope/telescope.nvim',
-		}
-	}
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require('onedark').setup {
+        -- Set a style preset. 'dark' is default.
+        style = 'darker', -- dark, darker, cool, deep, warm, warmer, light
+        colors = {
+          bg0 = '#17181b',
+        },
+      }
+      require('onedark').load()
+    end,
+  },
+
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
 }
