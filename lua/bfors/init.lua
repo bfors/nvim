f3ef9025b3ba8598b79c5dd5eda963502e159559
Brian Forsberg <bforsberg15@gmail.com>
2024-02-27 01:20:25 -0600

    Move inline function config to custom config section.

--- lua/bfors/init.lua
+++ lua/bfors/init.lua
@@ -1,52 +1,52 @@
 vim.opt.autochdir = false
-vim.keymap.set({ 'n' }, '<leader>c', [[:w<CR>:!cargo run<CR>]], { desc = '' })
-vim.keymap.set({ 'n' }, '<leader>p', [[:w<CR>:!python %<CR>]], { desc = '' })
-vim.keymap.set({ 'n' }, '<leader>t', [[:w<CR>:term python %<CR>]], { desc = '' })
-vim.keymap.set({ 'n' }, '<leader>fs', [[:w<CR>]], { desc = '' })
-vim.keymap.set({ 'n' }, '<leader>bp', [[:bp<CR>]], { desc = '' })
-vim.keymap.set({ 'n' }, '<leader>bn', [[:bn<CR>]], { desc = '' })
-vim.keymap.set({ 'n' }, '<leader>bd', [[:bd<CR>]], { desc = '' })
+vim.keymap.set({ "n" }, "<leader>c", [[:w<CR>:!cargo run<CR>]], { desc = "" })
+vim.keymap.set({ "n" }, "<leader>p", [[:w<CR>:!python %<CR>]], { desc = "" })
+vim.keymap.set({ "n" }, "<leader>t", [[:w<CR>:term python %<CR>]], { desc = "" })
+vim.keymap.set({ "n" }, "<leader>fs", [[:w<CR>]], { desc = "" })
+vim.keymap.set({ "n" }, "<leader>bp", [[:bp<CR>]], { desc = "" })
+vim.keymap.set({ "n" }, "<leader>bn", [[:bn<CR>]], { desc = "" })
+vim.keymap.set({ "n" }, "<leader>bd", [[:bd<CR>]], { desc = "" })
 
-local harpoon = require 'harpoon'
+local harpoon = require("harpoon")
 
 -- REQUIRED
-harpoon:setup {}
+harpoon:setup({})
 
 -- basic telescope configuration
-local conf = require('telescope.config').values
+local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
+	local file_paths = {}
+	for _, item in ipairs(harpoon_files.items) do
+		table.insert(file_paths, item.value)
+	end
 
-  require('telescope.pickers')
-    .new({}, {
-      prompt_title = 'Harpoon',
-      finder = require('telescope.finders').new_table {
-        results = file_paths,
-      },
-      previewer = conf.file_previewer {},
-      sorter = conf.generic_sorter {},
-    })
-    :find()
+	require("telescope.pickers")
+		.new({}, {
+			prompt_title = "Harpoon",
+			finder = require("telescope.finders").new_table({
+				results = file_paths,
+			}),
+			previewer = conf.file_previewer({}),
+			sorter = conf.generic_sorter({}),
+		})
+		:find()
 end
 
-vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
-vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 --
-vim.keymap.set('n', '<C-j>', function() harpoon:list():select(1) end)
-vim.keymap.set('n', '<C-k>', function() harpoon:list():select(2) end)
-vim.keymap.set('n', '<C-l>', function() harpoon:list():select(3) end)
-vim.keymap.set('n', '<C-;>', function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
 --
 -- -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end)
-vim.keymap.set('n', '<C-S-N>', function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
 -- Terminal mode binds
-vim.keymap.set('t', '<C-j>', function() harpoon:list():select(1) end)
-vim.keymap.set('t', '<C-k>', function() harpoon:list():select(2) end)
+vim.keymap.set("t", "<C-j>", function() harpoon:list():select(1) end)
+vim.keymap.set("t", "<C-k>", function() harpoon:list():select(2) end)
 -- vim.keymap.set("t", "<C-l>", function() harpoon:list():select(3) end)
-vim.keymap.set('t', '<C-;>', function() harpoon:list():select(4) end)
-vim.keymap.set({ 't' }, '<ESC>', [[<C-\><C-n>]], { desc = '' })
+vim.keymap.set("t", "<C-;>", function() harpoon:list():select(4) end)
+vim.keymap.set({ "t" }, "<ESC>", [[<C-\><C-n>]], { desc = "" })
